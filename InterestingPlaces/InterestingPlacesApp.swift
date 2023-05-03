8bca63310bb92f731e47c13ebfc3a70de88b849e
Nikita Rylskiy <todoskatebaby@gmail.com>
2023-05-03 19:33:52 +0300

    Initial Commit

---
+++ InterestingPlaces/InterestingPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestingPlacesApp.swift
+//  InterestingPlaces
+//
+//  Created by nikita on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InterestingPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
